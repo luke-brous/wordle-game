9623be638c3faa82edc92eb247aee76f409e0f20
luke-brous <luke.broussard@gmail.com>
2025-04-17 20:45:34 -0500

    framework

--- Test/WordleTest.java
+++ Test/WordleTest.java
@@ -1,3 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 public class WordleTest {
+
+
+
+
+
+    @Test
+    public void checkGuess_AllCorrect(){
+
+    }
 
 }
--- src/Wordle.java
+++ src/Wordle.java
@@ -1,8 +1,42 @@
+import java.util.ArrayList;
+
+
+
 public class Wordle {
-    public static void main(String[] args) {
-        StdOut.println("Hello");
-        StdOut.println("This is my game");
-        StdOut.println("Bleh");
+
+    private String targetWord;
+    ArrayList<String> guesses;
+
+
+
+
+    public Wordle(String answer){
+        this.targetWord = answer;
+        targetWord =
+
+    }
+
+
+    public static boolean isValidGuess(String guess){
+        if(guess.length() != 5) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public static ArrayList<String> checkGuess(String guess){
+        ArrayList<String> array = new ArrayList<String>(5);
+        return array;
+    }
+
+    public static boolean isGameOver(){
+        return false;
+    }
+
+    public static void reset(){
+
     }
 
 }
