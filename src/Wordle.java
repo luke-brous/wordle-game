8c178b4d50bd0309bfea5ccdb24f8df29de992f8
luke-brous <luke.broussard@gmail.com>
2025-04-23 16:14:11 -0500

    updated dictionary

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,23 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-
 
 
 public class Wordle {
 
     private String targetWord;
-    ArrayList<String> guesses;
+    private ArrayList<String> dictionary;
+    private ArrayList<String> guesses;
 
 
 
 
-    public Wordle(String answer){
+    public Wordle(String answer, ArrayList<String> dictionary){
         this.targetWord = answer;
+        this.dictionary = dictionary;
+
+    }
+
+    public static ArrayList<String> loadDictionary() {
+        ArrayList<String> words = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("5_letter_words.txt"))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                words.add(line.trim().toLowerCase());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        StdOut.println(words.toArray().length);
+        return words;
+
 
     }
 
 
-    public static boolean isValidGuess(String guess){
-        if(guess.length() != 5) {
+    public static boolean isValidGuess(String guess, ArrayList<String> dictionary){
+        if(guess.length() != 5 || !dictionary.contains(guess)) {
             return false;
         }
 
@@ -36,6 +56,11 @@ public class Wordle {
 
     public static void reset(){
 
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> dict = loadDictionary();
+        System.out.print(dict);
     }
 
 }
