f1c66be07ccb6bc688631cfe061c27aa7b7d6b3d
luke-brous <luke.broussard@gmail.com>
2025-04-28 16:07:20 -0500

    start on the UI

--- src/WordleGUI.java
+++ src/WordleGUI.java
@@ -1,10 +1,47 @@
+import java.awt.*;
+
 public class WordleGUI {
+
+
+    public static void main(String[] args){
+        draw();
+    }
 
 
 
     public static void draw(){
+        // Initialize necessary items
+        StdDraw.clear();
+        StdDraw.setXscale(-100, 100);
+        StdDraw.setYscale(-100, 100);
+        StdDraw.setCanvasSize(1024, 1024);
+        StdDraw.setPenColor(Color.BLACK);
+
+        // Build the grid
+        double x_cord = -40;
+        double y_cord = 85;
+        double boxSize = 9;
+
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 5; col++) {
+                double x = x_cord + col * 20;
+                double y = y_cord - row * 20;
+                StdDraw.square(x, y, boxSize);
+            }
+        }
+
+
+
+
+
+
+
+
+
 
     }
+
+
 
 
 
--- src/WordleModel.java
+++ src/WordleModel.java
@@ -1,3 +1,5 @@
+
+
 public class WordleModel {
     public static void main(String[] args) {
 
